524d047b43a5e32de4130eac3d96c5df8079731e
Carlos Celis <CelisTr@outlook.es>
2020-06-25 17:23:47 -0500

    About Section

--- Spa/src/app/app.module.ts
+++ Spa/src/app/app.module.ts
@@ -6,12 +6,14 @@ import { AppComponent } from './app.component';
 
 import { HomeComponent } from './home/home.component';
 import { HeaderComponent } from './home/header/header.component';
+import { AboutComponent } from './home/about/about.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    HeaderComponent
+    HeaderComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule,
