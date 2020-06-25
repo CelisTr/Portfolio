8e9cfe92d632aae78c05be9e0a5c1e74efb2d37f
Carlos Celis <CelisTr@outlook.es>
2020-06-25 11:32:44 -0500

    Header Section

--- Spa/src/app/app.module.ts
+++ Spa/src/app/app.module.ts
@@ -3,12 +3,15 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+
 import { HomeComponent } from './home/home.component';
+import { HeaderComponent } from './home/header/header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
@@ -17,4 +20,5 @@ import { HomeComponent } from './home/home.component';
   providers: [],
   bootstrap: [AppComponent]
 })
+
 export class AppModule { }
